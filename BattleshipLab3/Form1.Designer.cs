58cf0d8c91a5200da5119652e0f983a1fc6d5abe
Илья Ярмульский <ilaarmulskij119@gmail.com>
2026-02-04 21:43:34 +1000

    ui

--- BattleshipLab3/Form1.Designer.cs
+++ BattleshipLab3/Form1.Designer.cs
@@ -1,16 +1,23 @@
-﻿namespace BattleshipLab3;
+namespace BattleshipLab3;
 
 partial class Form1
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
     private System.ComponentModel.IContainer components = null;
+    private TableLayoutPanel playerGrid;
+    private TableLayoutPanel enemyGrid;
+    private Button btnHost;
+    private Button btnJoin;
+    private TextBox txtIp;
+    private NumericUpDown numPort;
+    private Label lblStatus;
+    private Button btnAutoPlace;
+    private Button btnStartGame;
+    private TextBox txtChatInput;
+    private Button btnSendChat;
+    private ListBox lstChat;
+    private Label lblPlayerField;
+    private Label lblEnemyField;
 
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
     protected override void Dispose(bool disposing)
     {
         if (disposing && (components != null))
@@ -20,19 +27,190 @@ partial class Form1
         base.Dispose(disposing);
     }
 
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        components = new System.ComponentModel.Container();
+        playerGrid = new TableLayoutPanel();
+        enemyGrid = new TableLayoutPanel();
+        btnHost = new Button();
+        btnJoin = new Button();
+        txtIp = new TextBox();
+        numPort = new NumericUpDown();
+        lblStatus = new Label();
+        btnAutoPlace = new Button();
+        btnStartGame = new Button();
+        txtChatInput = new TextBox();
+        btnSendChat = new Button();
+        lstChat = new ListBox();
+        lblPlayerField = new Label();
+        lblEnemyField = new Label();
+        ((System.ComponentModel.ISupportInitialize)numPort).BeginInit();
+        SuspendLayout();
+        // 
+        // playerGrid
+        // 
+        playerGrid.ColumnCount = 10;
+        playerGrid.RowCount = 10;
+        for (int i = 0; i < 10; i++)
+        {
+            playerGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            playerGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+        }
+        playerGrid.Location = new Point(12, 40);
+        playerGrid.Name = "playerGrid";
+        playerGrid.Size = new Size(300, 300);
+        playerGrid.TabIndex = 0;
+        // 
+        // enemyGrid
+        // 
+        enemyGrid.ColumnCount = 10;
+        enemyGrid.RowCount = 10;
+        for (int i = 0; i < 10; i++)
+        {
+            enemyGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            enemyGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+        }
+        enemyGrid.Location = new Point(330, 40);
+        enemyGrid.Name = "enemyGrid";
+        enemyGrid.Size = new Size(300, 300);
+        enemyGrid.TabIndex = 1;
+        // 
+        // btnHost
+        // 
+        btnHost.Location = new Point(12, 360);
+        btnHost.Name = "btnHost";
+        btnHost.Size = new Size(100, 30);
+        btnHost.TabIndex = 2;
+        btnHost.Text = "Host";
+        btnHost.UseVisualStyleBackColor = true;
+        btnHost.Click += btnHost_Click;
+        // 
+        // btnJoin
+        // 
+        btnJoin.Location = new Point(118, 360);
+        btnJoin.Name = "btnJoin";
+        btnJoin.Size = new Size(100, 30);
+        btnJoin.TabIndex = 3;
+        btnJoin.Text = "Join";
+        btnJoin.UseVisualStyleBackColor = true;
+        btnJoin.Click += btnJoin_Click;
+        // 
+        // txtIp
+        // 
+        txtIp.Location = new Point(224, 364);
+        txtIp.Name = "txtIp";
+        txtIp.PlaceholderText = "IP";
+        txtIp.Size = new Size(120, 23);
+        txtIp.TabIndex = 4;
+        // 
+        // numPort
+        // 
+        numPort.Location = new Point(350, 364);
+        numPort.Maximum = new decimal(new int[] { 65535, 0, 0, 0 });
+        numPort.Minimum = new decimal(new int[] { 1000, 0, 0, 0 });
+        numPort.Name = "numPort";
+        numPort.Size = new Size(80, 23);
+        numPort.TabIndex = 5;
+        numPort.Value = new decimal(new int[] { 9000, 0, 0, 0 });
+        // 
+        // lblStatus
+        // 
+        lblStatus.AutoSize = true;
+        lblStatus.Location = new Point(12, 9);
+        lblStatus.Name = "lblStatus";
+        lblStatus.Size = new Size(90, 15);
+        lblStatus.TabIndex = 6;
+        lblStatus.Text = "Ожидание игры";
+        // 
+        // btnAutoPlace
+        // 
+        btnAutoPlace.Location = new Point(450, 360);
+        btnAutoPlace.Name = "btnAutoPlace";
+        btnAutoPlace.Size = new Size(90, 30);
+        btnAutoPlace.TabIndex = 7;
+        btnAutoPlace.Text = "Автофлот";
+        btnAutoPlace.UseVisualStyleBackColor = true;
+        btnAutoPlace.Click += btnAutoPlace_Click;
+        // 
+        // btnStartGame
+        // 
+        btnStartGame.Location = new Point(546, 360);
+        btnStartGame.Name = "btnStartGame";
+        btnStartGame.Size = new Size(84, 30);
+        btnStartGame.TabIndex = 8;
+        btnStartGame.Text = "Старт";
+        btnStartGame.UseVisualStyleBackColor = true;
+        btnStartGame.Click += btnStartGame_Click;
+        // 
+        // txtChatInput
+        // 
+        txtChatInput.Location = new Point(12, 410);
+        txtChatInput.Name = "txtChatInput";
+        txtChatInput.Size = new Size(300, 23);
+        txtChatInput.TabIndex = 9;
+        // 
+        // btnSendChat
+        // 
+        btnSendChat.Location = new Point(318, 408);
+        btnSendChat.Name = "btnSendChat";
+        btnSendChat.Size = new Size(75, 27);
+        btnSendChat.TabIndex = 10;
+        btnSendChat.Text = "Отправить";
+        btnSendChat.UseVisualStyleBackColor = true;
+        btnSendChat.Click += btnSendChat_Click;
+        // 
+        // lstChat
+        // 
+        lstChat.FormattingEnabled = true;
+        lstChat.ItemHeight = 15;
+        lstChat.Location = new Point(630, 40);
+        lstChat.Name = "lstChat";
+        lstChat.Size = new Size(200, 319);
+        lstChat.TabIndex = 11;
+        // 
+        // lblPlayerField
+        // 
+        lblPlayerField.AutoSize = true;
+        lblPlayerField.Location = new Point(12, 22);
+        lblPlayerField.Name = "lblPlayerField";
+        lblPlayerField.Size = new Size(70, 15);
+        lblPlayerField.TabIndex = 12;
+        lblPlayerField.Text = "Ваше поле";
+        // 
+        // lblEnemyField
+        // 
+        lblEnemyField.AutoSize = true;
+        lblEnemyField.Location = new Point(330, 22);
+        lblEnemyField.Name = "lblEnemyField";
+        lblEnemyField.Size = new Size(75, 15);
+        lblEnemyField.TabIndex = 13;
+        lblEnemyField.Text = "Поле врага";
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(844, 451);
+        Controls.Add(lblEnemyField);
+        Controls.Add(lblPlayerField);
+        Controls.Add(lstChat);
+        Controls.Add(btnSendChat);
+        Controls.Add(txtChatInput);
+        Controls.Add(btnStartGame);
+        Controls.Add(btnAutoPlace);
+        Controls.Add(lblStatus);
+        Controls.Add(numPort);
+        Controls.Add(txtIp);
+        Controls.Add(btnJoin);
+        Controls.Add(btnHost);
+        Controls.Add(enemyGrid);
+        Controls.Add(playerGrid);
+        Name = "Form1";
+        Text = "Морской бой";
+        Load += Form1_Load;
+        ((System.ComponentModel.ISupportInitialize)numPort).EndInit();
+        ResumeLayout(false);
+        PerformLayout();
     }
-
-    #endregion
 }
+
--- BattleshipLab3/Form1.cs
+++ BattleshipLab3/Form1.cs
@@ -1,9 +1,371 @@
+using BattleshipLab3.Game;
+using BattleshipLab3.Models;
+using BattleshipLab3.Network;
+
 namespace BattleshipLab3;
 
 public partial class Form1 : Form
 {
+    private readonly Button[,] _playerButtons = new Button[Board.Size, Board.Size];
+    private readonly Button[,] _enemyButtons = new Button[Board.Size, Board.Size];
+
+    private GameState? _gameState;
+    private NetworkSession? _session;
+
     public Form1()
     {
         InitializeComponent();
+    }
+
+    private void Form1_Load(object? sender, EventArgs e)
+    {
+        BuildGrid(playerGrid, _playerButtons, PlayerCell_Click);
+        BuildGrid(enemyGrid, _enemyButtons, EnemyCell_Click);
+        SetUiEnabled(false);
+    }
+
+    private void BuildGrid(TableLayoutPanel grid, Button[,] buttons, EventHandler clickHandler)
+    {
+        grid.Controls.Clear();
+        grid.SuspendLayout();
+
+        for (int y = 0; y < Board.Size; y++)
+        {
+            for (int x = 0; x < Board.Size; x++)
+            {
+                var button = new Button
+                {
+                    Dock = DockStyle.Fill,
+                    Margin = new Padding(1),
+                    Tag = (x, y),
+                    BackColor = Color.LightBlue
+                };
+                button.Click += clickHandler;
+                buttons[x, y] = button;
+                grid.Controls.Add(button, x, y);
+            }
+        }
+
+        grid.ResumeLayout();
+    }
+
+    private async void btnHost_Click(object? sender, EventArgs e)
+    {
+        int port = (int)numPort.Value;
+        _gameState = new GameState(isHost: true);
+        _session = new NetworkSession(isHost: true, hostAddress: "0.0.0.0", port: port);
+        SubscribeToSession();
+
+        lblStatus.Text = "Вы хост. Ожидание клиента...";
+        await _session.StartAsync();
+        _gameState.StartPlacement();
+        SetUiEnabled(true);
+        lblStatus.Text = "Расставьте корабли и нажмите Старт";
+    }
+
+    private async void btnJoin_Click(object? sender, EventArgs e)
+    {
+        string ip = string.IsNullOrWhiteSpace(txtIp.Text) ? "127.0.0.1" : txtIp.Text.Trim();
+        int port = (int)numPort.Value;
+        _gameState = new GameState(isHost: false);
+        _session = new NetworkSession(isHost: false, hostAddress: ip, port: port);
+        SubscribeToSession();
+
+        lblStatus.Text = "Подключение к хосту...";
+        await _session.StartAsync();
+        _gameState.StartPlacement();
+        SetUiEnabled(true);
+        lblStatus.Text = "Расставьте корабли и ждите начала игры";
+    }
+
+    private void SubscribeToSession()
+    {
+        if (_session == null)
+            return;
+
+        _session.MessageReceived += OnNetworkMessageReceived;
+        _session.ConnectionStatusChanged += text =>
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(() => lblStatus.Text = text));
+            }
+            else
+            {
+                lblStatus.Text = text;
+            }
+        };
+    }
+
+    private void SetUiEnabled(bool afterConnection)
+    {
+        btnAutoPlace.Enabled = afterConnection;
+        btnStartGame.Enabled = afterConnection;
+        enemyGrid.Enabled = false;
+    }
+
+    private void PlayerCell_Click(object? sender, EventArgs e)
+    {
+        if (_gameState == null)
+            return;
+
+        if (_gameState.Phase != GamePhase.Placement)
+            return;
+
+        // В этой реализации ручная расстановка не делается,
+        // используем кнопку "Автофлот".
+    }
+
+    private async void EnemyCell_Click(object? sender, EventArgs e)
+    {
+        if (_gameState == null || _session == null)
+            return;
+
+        if (_gameState.Phase != GamePhase.MyTurn)
+            return;
+
+        if (sender is not Button button)
+            return;
+
+        var (x, y) = ((int x, int y))button.Tag;
+
+        await _session.SendAsync(new NetworkMessage
+        {
+            Type = MessageType.Shot,
+            X = x,
+            Y = y
+        });
+
+        _gameState.SetPhase(GamePhase.EnemyTurn);
+        UpdateStatusText();
+    }
+
+    private void btnAutoPlace_Click(object? sender, EventArgs e)
+    {
+        if (_gameState == null)
+            return;
+
+        _gameState.MyBoard.AutoPlaceStandardFleet();
+        RedrawBoards();
+    }
+
+    private async void btnStartGame_Click(object? sender, EventArgs e)
+    {
+        if (_gameState == null || _session == null)
+            return;
+
+        if (_gameState.Phase != GamePhase.Placement)
+            return;
+
+        await _session.SendAsync(new NetworkMessage
+        {
+            Type = MessageType.StartGame
+        });
+
+        if (_gameState.IsHost)
+            _gameState.StartGame(myTurnFirst: true);
+
+        enemyGrid.Enabled = _gameState.Phase == GamePhase.MyTurn;
+        UpdateStatusText();
+    }
+
+    private async void btnSendChat_Click(object? sender, EventArgs e)
+    {
+        if (_session == null)
+            return;
+
+        string text = txtChatInput.Text.Trim();
+        if (string.IsNullOrEmpty(text))
+            return;
+
+        txtChatInput.Clear();
+        lstChat.Items.Add("Вы: " + text);
+
+        await _session.SendAsync(new NetworkMessage
+        {
+            Type = MessageType.Chat,
+            Message = text
+        });
+    }
+
+    private void OnNetworkMessageReceived(NetworkMessage message)
+    {
+        if (InvokeRequired)
+        {
+            BeginInvoke(new Action(() => OnNetworkMessageReceived(message)));
+            return;
+        }
+
+        if (_gameState == null)
+            return;
+
+        switch (message.Type)
+        {
+            case MessageType.StartGame:
+                if (!_gameState.IsHost)
+                    _gameState.StartGame(myTurnFirst: false);
+                enemyGrid.Enabled = _gameState.Phase == GamePhase.MyTurn;
+                UpdateStatusText();
+                break;
+
+            case MessageType.Shot:
+                HandleIncomingShot(message);
+                break;
+
+            case MessageType.ShotResult:
+                HandleShotResult(message);
+                break;
+
+            case MessageType.Chat:
+                if (!string.IsNullOrEmpty(message.Message))
+                    lstChat.Items.Add("Соперник: " + message.Message);
+                break;
+        }
+    }
+
+    private async void HandleIncomingShot(NetworkMessage message)
+    {
+        if (_gameState == null || _session == null)
+            return;
+
+        if (message.X == null || message.Y == null)
+            return;
+
+        int x = message.X.Value;
+        int y = message.Y.Value;
+
+        var result = _gameState.MyBoard.ShootAt(x, y);
+        RedrawBoards();
+
+        await _session.SendAsync(new NetworkMessage
+        {
+            Type = MessageType.ShotResult,
+            X = x,
+            Y = y,
+            Outcome = result.Outcome.ToString()
+        });
+
+        if (_gameState.MyBoard.AllShipsSunk())
+        {
+            _gameState.FinishGame();
+            lblStatus.Text = "Поражение";
+            enemyGrid.Enabled = false;
+            return;
+        }
+
+        if (result.Outcome == ShotOutcome.Miss || result.Outcome == ShotOutcome.Repeated)
+        {
+            _gameState.SetPhase(GamePhase.MyTurn);
+            enemyGrid.Enabled = true;
+        }
+        else
+        {
+            _gameState.SetPhase(GamePhase.EnemyTurn);
+        }
+
+        UpdateStatusText();
+    }
+
+    private void HandleShotResult(NetworkMessage message)
+    {
+        if (_gameState == null)
+            return;
+
+        if (message.X == null || message.Y == null || string.IsNullOrEmpty(message.Outcome))
+            return;
+
+        int x = message.X.Value;
+        int y = message.Y.Value;
+
+        if (!Enum.TryParse<ShotOutcome>(message.Outcome, out var outcome))
+            return;
+
+        var current = _gameState.EnemyBoard[x, y];
+        switch (outcome)
+        {
+            case ShotOutcome.Miss:
+                if (current == CellState.Empty)
+                    SetEnemyCellState(x, y, CellState.Miss);
+                _gameState.SetPhase(GamePhase.EnemyTurn);
+                enemyGrid.Enabled = false;
+                break;
+            case ShotOutcome.Hit:
+                SetEnemyCellState(x, y, CellState.Hit);
+                _gameState.SetPhase(GamePhase.MyTurn);
+                enemyGrid.Enabled = true;
+                break;
+            case ShotOutcome.Sunk:
+                SetEnemyCellState(x, y, CellState.Sunk);
+                _gameState.SetPhase(GamePhase.MyTurn);
+                enemyGrid.Enabled = true;
+                break;
+            case ShotOutcome.Repeated:
+                _gameState.SetPhase(GamePhase.EnemyTurn);
+                enemyGrid.Enabled = false;
+                break;
+        }
+
+        if (_gameState.EnemyBoard.AllShipsSunk())
+        {
+            _gameState.FinishGame();
+            enemyGrid.Enabled = false;
+            lblStatus.Text = "Победа";
+            return;
+        }
+
+        UpdateStatusText();
+    }
+
+    private void SetEnemyCellState(int x, int y, CellState state)
+    {
+        var button = _enemyButtons[x, y];
+        button.BackColor = state switch
+        {
+            CellState.Miss => Color.LightGray,
+            CellState.Hit => Color.OrangeRed,
+            CellState.Sunk => Color.DarkRed,
+            _ => button.BackColor
+        };
+    }
+
+    private void RedrawBoards()
+    {
+        if (_gameState == null)
+            return;
+
+        for (int x = 0; x < Board.Size; x++)
+        {
+            for (int y = 0; y < Board.Size; y++)
+            {
+                var myCell = _gameState.MyBoard[x, y];
+                var myButton = _playerButtons[x, y];
+
+                myButton.BackColor = myCell switch
+                {
+                    CellState.Empty => Color.LightBlue,
+                    CellState.Ship => Color.SteelBlue,
+                    CellState.Miss => Color.LightGray,
+                    CellState.Hit => Color.OrangeRed,
+                    CellState.Sunk => Color.DarkRed,
+                    _ => myButton.BackColor
+                };
+            }
+        }
+    }
+
+    private void UpdateStatusText()
+    {
+        if (_gameState == null)
+            return;
+
+        lblStatus.Text = _gameState.Phase switch
+        {
+            GamePhase.WaitingForConnection => "Ожидание подключения",
+            GamePhase.Placement => "Расстановка флота",
+            GamePhase.MyTurn => "Ваш ход",
+            GamePhase.EnemyTurn => "Ход соперника",
+            GamePhase.GameOver => "Игра окончена",
+            _ => lblStatus.Text
+        };
     }
 }
